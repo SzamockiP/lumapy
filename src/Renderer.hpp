c0375c210500d50437ee95c9f856a1e3aab3e8a1
SzamockiP <szamocki.piotr04@gmail.com>
2026-07-03 22:42:49 +0200

    Switched to vkbootstrap

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <volk.h>
-#include <ranges>
-#include <algorithm>
-#include <span>
+#include <VkBootstrap.h>
+
 #include <expected>
 #include <memory>
+#include <vector>
+#include <string>
 
 #include "Logger.hpp"
 #include "window.hpp"
@@ -12,11 +13,8 @@
 class Renderer
 {
 private:
-	Renderer(Logger& logger, Window& window, VkInstance instance, VkDebugUtilsMessengerEXT debug_messenger) :
-		logger_(logger), window_(window), instance_(instance), debug_messenger_(debug_messenger)
-	{
-		logger_.log("Vulkan: Initialized");
-	}
+	Renderer(Logger& logger, Window& window)
+		: logger_(logger), window_(window) {}
 
 public:
 	static std::expected<std::unique_ptr<Renderer>, std::string> create(Logger& logger, Window& window)
@@ -26,96 +24,139 @@ public:
 			return std::unexpected("Vulkan: Failed to initialize volk");
 		}
 
-		auto instance_res = create_instance();
-		if (!instance_res)
+		auto renderer = std::unique_ptr<Renderer>(new Renderer(logger, window));
+
+		// Instance + Debug Messenger 
+		auto inst_ret = vkb::InstanceBuilder{}
+			.set_app_name("LumaPy Engine")
+			.set_app_version(1, 0, 0)
+			.require_api_version(1, 3, 0)
+			.request_validation_layers()
+			.set_debug_callback(debug_callback)
+			.set_debug_callback_user_data_pointer(&logger)
+			.set_debug_messenger_severity(
+				VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+				VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+			.set_debug_messenger_type(
+				VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+				VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+				VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT)
+			.build();
+
+		if (!inst_ret)
 		{
-			return std::unexpected(instance_res.error());
+			return std::unexpected("Vulkan: " + inst_ret.error().message());
 		}
-		VkInstance instance = *instance_res;
+		renderer->vkb_instance_ = inst_ret.value();
+		volkLoadInstance(renderer->vkb_instance_.instance);
 
-		volkLoadInstance(instance);
-
-		auto messenger_res = setup_debug_messenger(instance, logger);
-		if (!messenger_res)
+		// Surface 
+		VkSurfaceKHR surface = VK_NULL_HANDLE;
+		if (glfwCreateWindowSurface(renderer->vkb_instance_.instance,
+				window.get_native_handle(), nullptr, &surface) != VK_SUCCESS)
 		{
-			vkDestroyInstance(instance, nullptr);
-			return std::unexpected(messenger_res.error());
+			return std::unexpected("Vulkan: Failed to create window surface");
 		}
-		VkDebugUtilsMessengerEXT debug_messenger = *messenger_res;
+		renderer->surface_ = surface;
 
-		return std::unique_ptr<Renderer>(new Renderer(logger, window, instance, debug_messenger));
+		// Physical Device
+		auto phys_ret = vkb::PhysicalDeviceSelector{ renderer->vkb_instance_ }
+			.set_surface(surface)
+			.set_minimum_version(1, 3)
+			.prefer_gpu_device_type(vkb::PreferredDeviceType::discrete)
+			.select();
+
+		if (!phys_ret)
+		{
+			return std::unexpected("Vulkan: " + phys_ret.error().message());
+		}
+		renderer->vkb_physical_device_ = phys_ret.value();
+
+		// Logical Device + Queues
+		auto dev_ret = vkb::DeviceBuilder{ renderer->vkb_physical_device_ }.build();
+
+		if (!dev_ret)
+		{
+			return std::unexpected("Vulkan: " + dev_ret.error().message());
+		}
+		renderer->vkb_device_ = dev_ret.value();
+		volkLoadDevice(renderer->vkb_device_.device);
+
+		auto gq = renderer->vkb_device_.get_queue(vkb::QueueType::graphics);
+		if (!gq)
+		{
+			return std::unexpected("Vulkan: Failed to get graphics queue");
+		}
+		renderer->graphics_queue_ = gq.value();
+		renderer->graphics_queue_family_ =
+			renderer->vkb_device_.get_queue_index(vkb::QueueType::graphics).value();
+
+		auto pq = renderer->vkb_device_.get_queue(vkb::QueueType::present);
+		if (!pq)
+		{
+			return std::unexpected("Vulkan: Failed to get present queue");
+		}
+		renderer->present_queue_ = pq.value();
+
+		// Swapchain
+		auto sc_ret = vkb::SwapchainBuilder{ renderer->vkb_device_ }
+			.set_desired_present_mode(VK_PRESENT_MODE_MAILBOX_KHR)
+			.add_fallback_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+			.build();
+
+		if (!sc_ret)
+		{
+			return std::unexpected("Vulkan: " + sc_ret.error().message());
+		}
+		renderer->vkb_swapchain_ = sc_ret.value();
+		renderer->swapchain_images_ = renderer->vkb_swapchain_.get_images().value();
+		renderer->swapchain_image_views_ = renderer->vkb_swapchain_.get_image_views().value();
+
+		logger.log("Vulkan: Initialized (" +
+			std::string(renderer->vkb_physical_device_.properties.deviceName) + ")");
+
+		return renderer;
 	}
 
 	~Renderer()
 	{
-		if (debug_messenger_)
+		if (vkb_device_.device)
 		{
-			auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance_, "vkDestroyDebugUtilsMessengerEXT");
-			if (func != nullptr)
-			{
-				func(instance_, debug_messenger_, nullptr);
-			}
+			vkDeviceWaitIdle(vkb_device_.device);
 		}
 
-		if (instance_)
+		for (auto iv : swapchain_image_views_)
 		{
-			vkDestroyInstance(instance_, nullptr);
+			vkDestroyImageView(vkb_device_.device, iv, nullptr);
 		}
+
+		vkb::destroy_swapchain(vkb_swapchain_);
+
+		if (surface_)
+		{
+			vkDestroySurfaceKHR(vkb_instance_.instance, surface_, nullptr);
+		}
+
+		vkb::destroy_device(vkb_device_);
+		vkb::destroy_instance(vkb_instance_);
 	}
 
 	Renderer(const Renderer&) = delete;
 	Renderer& operator=(const Renderer&) = delete;
 
-	static std::expected<VkInstance, std::string> create_instance()
-	{
-		if (!check_validation_layer_support())
-		{
-			return std::unexpected("Vulkan: Required validation layers are not available");
-		}
+	VkInstance instance() const { return vkb_instance_.instance; }
+	VkDevice device() const { return vkb_device_.device; }
+	VkPhysicalDevice physical_device() const { return vkb_physical_device_.physical_device; }
+	VkQueue graphics_queue() const { return graphics_queue_; }
+	VkQueue present_queue() const { return present_queue_; }
+	std::uint32_t graphics_queue_family() const { return graphics_queue_family_; }
+	VkSwapchainKHR swapchain() const { return vkb_swapchain_.swapchain; }
+	VkFormat swapchain_format() const { return vkb_swapchain_.image_format; }
+	VkExtent2D swapchain_extent() const { return vkb_swapchain_.extent; }
+	const std::vector<VkImage>& swapchain_images() const { return swapchain_images_; }
+	const std::vector<VkImageView>& swapchain_image_views() const { return swapchain_image_views_; }
 
-		VkApplicationInfo application_info{
-			.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-			.pApplicationName = "LumaPy Engine",
-			.applicationVersion = VK_MAKE_VERSION(1,0,0),
-			.pEngineName = "No Engine",
-			.engineVersion = VK_MAKE_VERSION(1,0,0),
-			.apiVersion = VK_API_VERSION_1_3
-		};
-
-		auto extensions = get_required_extensions();
-
-		VkInstanceCreateInfo create_info{
-			.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-			.pApplicationInfo = &application_info,
-			.enabledLayerCount = static_cast<std::uint32_t>(validation_layers_.size()),
-			.ppEnabledLayerNames = validation_layers_.data(),
-			.enabledExtensionCount = static_cast<std::uint32_t>(extensions.size()),
-			.ppEnabledExtensionNames = extensions.data(),
-		};
-
-		VkInstance instance = VK_NULL_HANDLE;
-		if (vkCreateInstance(&create_info, nullptr, &instance) != VK_SUCCESS)
-		{
-			return std::unexpected("Vulkan: Failed to create an instance");
-		}
-		return instance;
-	}
-
-	static std::vector<const char*> get_required_extensions()
-	{
-		std::uint32_t glfw_extension_count{ 0 };
-		const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
-
-		std::vector<const char*> extensions(
-			std::span(glfw_extensions, glfw_extension_count).begin(),
-			std::span(glfw_extensions, glfw_extension_count).end()
-		);
-
-		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-
-		return extensions;
-	}
-
+private:
 	static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(
 		VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
 		VkDebugUtilsMessageTypeFlagsEXT message_type,
@@ -134,64 +175,25 @@ public:
 			{
 				prefix += "WARNING: ";
 			}
-			
+
 			logger->log(prefix + callback_data->pMessage);
 		}
 		return VK_FALSE;
 	}
 
-	static bool check_validation_layer_support()
-	{
-		std::uint32_t layer_count;
-		vkEnumerateInstanceLayerProperties(&layer_count, nullptr);
-		std::vector<VkLayerProperties> available_layers(layer_count);
-		vkEnumerateInstanceLayerProperties(&layer_count, available_layers.data());
-
-		auto available_names = available_layers
-			| std::views::transform([](const VkLayerProperties& prop)
-				{
-					return std::string_view(prop.layerName);
-				}
-			);
-
-		return std::ranges::all_of(validation_layers_, [&available_names](std::string_view required_layer)
-			{
-				return std::ranges::contains(available_names, std::string_view(required_layer));
-			}
-		);
-	}
-
-	static std::expected<VkDebugUtilsMessengerEXT, std::string> setup_debug_messenger(VkInstance instance, Logger& logger)
-	{
-		VkDebugUtilsMessengerCreateInfoEXT create_info
-		{
-			.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
-			.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT,
-			.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
-			.pfnUserCallback = debug_callback,
-			.pUserData = &logger
-		};
-		
-		auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
-		if (func != nullptr)
-		{
-			VkDebugUtilsMessengerEXT debug_messenger = VK_NULL_HANDLE;
-			if (func(instance, &create_info, nullptr, &debug_messenger) != VK_SUCCESS)
-			{
-				return std::unexpected("Vulkan: Failed to connect debug messenger");
-			}
-			return debug_messenger;
-		}
-		return VK_NULL_HANDLE;
-	}
-
-private:
 	Logger& logger_;
 	Window& window_;
-	VkInstance instance_ = VK_NULL_HANDLE;
-	VkDebugUtilsMessengerEXT debug_messenger_ = VK_NULL_HANDLE;
 
-	static inline const std::vector<const char*> validation_layers_ = {
-		"VK_LAYER_KHRONOS_validation"
-	};
+	vkb::Instance vkb_instance_;
+	vkb::PhysicalDevice vkb_physical_device_;
+	vkb::Device vkb_device_;
+	vkb::Swapchain vkb_swapchain_;
+
+	VkSurfaceKHR surface_ = VK_NULL_HANDLE;
+	VkQueue graphics_queue_ = VK_NULL_HANDLE;
+	VkQueue present_queue_ = VK_NULL_HANDLE;
+	std::uint32_t graphics_queue_family_ = 0;
+
+	std::vector<VkImage> swapchain_images_;
+	std::vector<VkImageView> swapchain_image_views_;
 };
